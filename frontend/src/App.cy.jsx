e383c40014dffb41486be52a62f4d88fe2f3d632
LehetaVasylyna <leheta.vasylyna@student.uzhnu.edu.ua>
2024-10-18 20:27:39 +0300

    Added husky, pre-commit, cypress tests

--- frontend/src/App.cy.jsx
+++ frontend/src/App.cy.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import { App } from './App'
+import React from 'react';
+
+import App from './App';
 
 describe('<App />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<App />)
-  })
-})
+    it('renders', () => {
+        // see: https://on.cypress.io/mounting-react
+        cy.mount(<App />);
+        cy.getByTestId('app').should('exist').and('be.visible');
+    });
+});
