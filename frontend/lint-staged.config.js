09eab2f6efda4d09c5dd0580156a143e5df58573
LehetaVasylyna <leheta.vasylyna@student.uzhnu.edu.ua>
2024-10-22 02:35:52 +0300

    Test pre-commit

--- frontend/lint-staged.config.js
+++ frontend/lint-staged.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     '*.{js,jsx}': ['eslint --ignore-path .gitignore --fix --ext .js,.jsx --max-warnings 0 ./src'],
     '*.{js,jsx,css,mjs}': ['prettier --write'],
+    '*.{js,json,css,scss,html,md}': ['prettier --write', 'git add'],
 };
