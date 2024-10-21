de72de98a736a5f5ca533e0775eb949cb8c07ced
LehetaVasylyna <leheta.vasylyna@student.uzhnu.edu.ua>
2024-10-21 09:58:02 +0300

    "Testing hooks"

--- frontend/src/bootstrap.js
+++ frontend/src/bootstrap.js
@@ -4,9 +4,6 @@ import { createRoot } from 'react-dom/client';
 // import { App } from './App';
 import App from './App';
 
-let c = 12;
-console.log(c);
-
 const domNode = document.querySelector('#react-app');
 const root = createRoot(domNode);
 
