ede0b5ed48958102b16074c58a78b6c884b617a1
LehetaVasylyna <leheta.vasylyna@student.uzhnu.edu.ua>
2024-10-22 03:20:26 +0300

    Test pre-commit

--- frontend/.husky/install.mjs
+++
@@ -1,9 +0,0 @@
-// Skip Husky install in production and CI
-
-if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
-    process.exit(0);
-}
-(async () => {
-    const husky = (await require('husky')).default;
-    console.log(husky());
-})();
