14b919f21b8aeaa9a1adb10f23ba15486ee96829
LehetaVasylyna <leheta.vasylyna@student.uzhnu.edu.ua>
2024-10-22 15:54:30 +0300

    Test pre-commit

--- frontend/.husky/install.mjs
+++ frontend/.husky/install.mjs
@@ -2,6 +2,7 @@
 if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
     process.exit(0);
 }
+
 async function myFunction() {
     const husky = await husky.default;
     console.log(husky());
